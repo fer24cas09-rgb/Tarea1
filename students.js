0a0f81c7b665294917cfa2b1e209d6db7a9106c9
fer24cas09-rgb <fer24cas09@gmail.com>
2026-09-11 18:42:33 -0600

    Refactor student form handling and display logic
    Refactor code to improve readability anActualización de función mostrarAlumnos)

--- students.js
+++ students.js
@@ -1,4 +1,4 @@
-// Obtener elementos del HTML
+
 const formulario = document.getElementById("formulario");
 const numeroControl = document.getElementById("numeroControl");
 const nombre = document.getElementById("nombre");
@@ -6,46 +6,46 @@ const carrera = document.getElementById("carrera");
 const lista = document.getElementById("lista");
 
 
-// Obtener los alumnos guardados
+
 let alumnos = JSON.parse(localStorage.getItem("alumnos")) || [];
 
 
-// Mostrar los alumnos al cargar la página
+
 mostrarAlumnos();
 
 
-// Evento del formulario
+
 formulario.addEventListener("submit", function (evento) {
 
-    // Evitar que la página se recargue
+    
     evento.preventDefault();
 
-    // Crear un objeto alumno
+    
     const alumno = {
         numeroControl: numeroControl.value,
         nombre: nombre.value,
         carrera: carrera.value
     };
 
-    // Agregar el alumno al arreglo
+    
     alumnos.push(alumno);
 
-    // Guardar en localStorage
+    
     localStorage.setItem(
         "alumnos",
         JSON.stringify(alumnos)
     );
 
-    // Mostrar nuevamente la lista
+    
     mostrarAlumnos();
 
-    // Limpiar formulario
+
     formulario.reset();
 
 });
 
 
-// Función para mostrar alumnos
+
 function mostrarAlumnos() {
 
     lista.innerHTML = "";
